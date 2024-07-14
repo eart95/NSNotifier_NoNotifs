1cc1adbc98ca552b3fe19cf98c75533ccc57d31e
eart95 <eart95@gmail.com>
2024-07-14 21:11:22 +0100

    Create NightscoutNotifierTests.swift

---
+++ Tests/NightscoutNotifierTests/NightscoutNotifierTests.swift
@@ -0,0 +1,10 @@
+// Tests/NightscoutNotifierTests/NightscoutNotifierTests.swift
+
+import XCTest
+@testable import NightscoutNotifier
+
+final class NightscoutNotifierTests: XCTestCase {
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+}
